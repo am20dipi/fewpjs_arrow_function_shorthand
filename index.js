8ccef458c9131e736a523d9ad5ae1c198122969f
Adriana <am20dipi@alum.siena.edu>
2021-06-22 14:18:27 -0400

    commit tree 824d1a394ee49c4fa59fdafd42371d837113315b

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function() {
+    let sum = 2000/100
+    return sum
+}
+
+let square = (num) => {
+    let sum = num * num
+    return sum
+}
+
+let add = (first, second) => {
+    let sum = first + second
+    return sum
+}
